01a1df7944c433c30bc295c2b9cce7c30d889a3e
Ziad Baroudi <ziad_baroudi@hotmail.com>
2021-04-30 02:23:24 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
-const testVar = {}
+const testVar = {};
+function superbowlWin(arr){
+  let a = arr.find(r => r.result ==="W");
 
+  if(!!a === true){
+    return a.year;
+  }
+  else {
+    return undefined;
+  }
+}
 function testFunc() {
-  return "hi"
+  return "hi";
 }
